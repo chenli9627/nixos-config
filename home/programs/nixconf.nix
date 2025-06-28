eab30c5373cb23f59dcd5056bd47ddc7a54077d7
chenli9627 <chenli9627@gmail.com>
2025-06-29 05:00:45 +0800

    wkekl

--- home/programs/nixconf.nix
+++ home/programs/nixconf.nix
@@ -1,5 +1,5 @@
 {...}: {
   nix.extraOptions = ''
-    access-tokens = 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
+    access-tokens = github.com=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
   '';
 }
