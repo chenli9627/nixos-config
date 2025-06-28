52c7a752dea6fafebf5ba9a15455de2db985cfd5
chenli9627 <chenli9627@gmail.com>
2025-06-29 04:26:26 +0800

    some fish config

--- home/programs/fish.nix
+++ home/programs/fish.nix
@@ -1,10 +1,17 @@
 {pkgs, ...}: {
   programs.fish = {
     enable = true;
+    generateCompletions = true;
+    preferAbbrs = true;
     shellAbbrs = {
       lzg = "lazygit";
       lzd = "lazydocker";
       n = "nvim";
+      :q = "exit";
     };
+    shellInit = ''
+      direnv hook fish | source
+      set -g direnv_fish_mode eval_after_arrow
+      '';
   };
 }
