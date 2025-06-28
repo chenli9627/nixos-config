c58b0438009c582785612d79dc72cb4a1ee68c65
chenli9627 <chenli9627@gmail.com>
2025-06-29 00:41:35 +0800

    some zellij settings

--- home/programs/zellij.nix
+++ home/programs/zellij.nix
@@ -1,12 +1,13 @@
 {pkgs,...}:{
   programs.zellij= {
     enable = true;
-    enableBashIntegration = true;
-    enableFishIntegration = true;
+    # enableBashIntegration = true;
+    # enableFishIntegration = true;
     settings = {
       # theme = "dracula";
       theme = "catppuccin-latte";
       auto_start = false;
+      default_layout = "compact";
     };
   };
 }
