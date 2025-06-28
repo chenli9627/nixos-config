0832e7ad249b4e4369aee9dcf244aaffe6b19890
chenli9627 <chenli9627@gmail.com>
2025-06-29 00:26:15 +0800

    sdk

--- home/programs/zellij.nix
+++ home/programs/zellij.nix
@@ -3,5 +3,11 @@
     enable = true;
     enableBashIntegration = true;
     enableFishIntegration = true;
+    # themes = [
+    #   catppuccin-latte
+    # ];
+    settings = {
+      themes  ="catppuccin"
+    };
   };
 }
