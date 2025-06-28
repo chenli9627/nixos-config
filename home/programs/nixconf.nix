52fc46febc697ca4ef9dcef4552cce402187be8a
chenli9627 <chenli9627@gmail.com>
2025-06-29 05:00:07 +0800

    dks

--- home/programs/nixconf.nix
+++ home/programs/nixconf.nix
@@ -1,7 +1,5 @@
 {...}: {
-  nix.settings = {
-    access-extraOptions = ''
-      access-tokens = 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
-    '';
-  };
+  nix.extraOptions = ''
+    access-tokens = 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
+  '';
 }
