3a2d8112445a392a5ad6341cb289749df0ae83c2
chenli9627 <chenli9627@gmail.com>
2025-06-29 22:25:43 +0800

    ewko

--- hosts/lenovo-laptop/default.nix
+++ hosts/lenovo-laptop/default.nix
@@ -12,6 +12,7 @@
     ../../modules/gnome.nix
     ../../modules/nix-ld.nix
     ../../modules/virtualisation.nix
+    ../../modules/keyd.nix
 
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
