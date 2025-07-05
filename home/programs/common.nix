8cd3ec72df6168619e68c691f1a50a979d6be5f0
chenli9627 <chenli9627@gmail.com>
2025-07-06 01:25:07 +0800

    add bat

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -69,6 +69,7 @@
     p7zip
     efibootmgr
     acpi
+    bat
     ripgrep # recursively searches directories for a regex pattern
     jq # A lightweight and flexible command-line JSON processor
     yq-go # yaml processor https://github.com/mikefarah/yq
