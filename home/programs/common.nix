47cd2cd95b40a7b55185d9c351b739d229c609d6
chenli9627 <chenli9627@gmail.com>
2025-07-06 15:55:10 +0800

    add fastfetch

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -70,6 +70,7 @@
     efibootmgr
     acpi
     bat
+    fastfetch
     ripgrep # recursively searches directories for a regex pattern
     jq # A lightweight and flexible command-line JSON processor
     yq-go # yaml processor https://github.com/mikefarah/yq
