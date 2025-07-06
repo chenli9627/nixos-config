9fd7548b63819daaf698cfbd64f23fd18f6e07a7
chenli9627 <chenli9627@gmail.com>
2025-07-07 01:24:47 +0800

    woidj

--- flake.nix
+++ flake.nix
@@ -24,14 +24,24 @@
           inherit specialArgs;
           system = "x86_64-linux";
           modules = [
+            # ./configuration.nix
             ./hosts/lenovo-laptop
+            # ./users/chen
+
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+
+              # home-manager.users.chen = ./home.nix;
               home-manager.users.${username} = import ./users/${username}/home.nix;
+              # home-manager.users.username = import ./users/username/home.nix;
+
+              # 取消注释下面这一行，就可以在 home.nix 中使用 flake 的所有 inputs 参数了
+              # home-manager.extraSpecialArgs = inputs // specialArgs;
               home-manager.extraSpecialArgs = {
-                inherit (inputs) nixpkgs home-manager hyprland;
+                # inherit (inputs) nixpkgs home-manager hyprland;
+                inherit inputs;
                 inherit username;
               };
             }
