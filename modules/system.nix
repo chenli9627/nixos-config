86ee1f736ce1e9a4c916dd276c89cf833ebf23e0
chenli9627 <chenli9627@gmail.com>
2025-07-07 01:57:44 +0800

    wijd

--- modules/system.nix
+++ modules/system.nix
@@ -21,13 +21,12 @@
   users.extraGroups.vboxusers.members = [username];
 
   nix.settings = {
-    trusted-users = [username];
+    trusted-users = [username root];
     substituters = [
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://hyprland.cachix.org"
     ];
-    trusted-substituters = ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     experimental-features = "nix-command flakes";
     builders-use-substitutes = true;
