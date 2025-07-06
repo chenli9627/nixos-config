a8c3cbc0c2914f2d9fbeff30e4f78f9759b345dc
chenli9627 <chenli9627@gmail.com>
2025-07-06 16:57:06 +0800

    add hyprland cachix

--- modules/system.nix
+++ modules/system.nix
@@ -4,8 +4,7 @@
   username,
   lib,
   ...
-}:
-{
+}: {
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${username} = {
     isNormalUser = true;
@@ -19,15 +18,18 @@
     #   tree
     # ];
   };
-  users.extraGroups.vboxusers.members = [ username ];
+  users.extraGroups.vboxusers.members = [username];
 
   # nix.settings.trusted-users = [username];
   nix.settings = {
-    trusted-users = [ username ];
+    trusted-users = [username];
     substituters = [
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://hyprland.cachix.org"
     ];
+    trusted-substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     experimental-features = "nix-command flakes";
     builders-use-substitutes = true;
   };
