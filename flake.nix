25396f488eebb8fe5d98c91b824f9c3fc6b99162
chenli9627 <chenli9627@gmail.com>
2025-07-07 02:21:16 +0800

    weiodsj

--- flake.nix
+++ flake.nix
@@ -41,8 +41,7 @@
               # home-manager.extraSpecialArgs = inputs // specialArgs;
               home-manager.extraSpecialArgs = {
                 # inherit (inputs) nixpkgs home-manager hyprland;
-                inherit inputs;
-                inherit username;
+                inherit inputs username;
               };
             }
           ];
