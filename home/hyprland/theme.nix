28f59dade7a177ca06e4c6fbfcefeb4604f8f629
chenli9627 <chenli9627@gmail.com>
2025-07-07 16:22:06 +0800

    qwpoe

--- home/hyprland/theme.nix
+++ home/hyprland/theme.nix
@@ -5,13 +5,13 @@
     enable = true;
     extraPortals = [
       pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-hyprland
+      # pkgs.xdg-desktop-portal-hyprland
       # pkgs.xdg-desktop-portal-wlr
       # pkgs.xdg-desktop-portal-gnome
     ];
     configPackages = [
       pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-hyprland
+      # pkgs.xdg-desktop-portal-hyprland
       # pkgs.xdg-desktop-portal-wlr
       # pkgs.xdg-desktop-portal-gnome
     ];
