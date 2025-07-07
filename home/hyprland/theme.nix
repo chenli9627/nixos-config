c230dee4fac0d3a5ce157f5114279020cc1e186c
chenli9627 <chenli9627@gmail.com>
2025-07-07 16:20:57 +0800

    woodsi

--- home/hyprland/theme.nix
+++ home/hyprland/theme.nix
@@ -6,14 +6,14 @@
     extraPortals = [
       pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-hyprland
-      pkgs.xdg-desktop-portal-wlr
-      pkgs.xdg-desktop-portal-gnome
+      # pkgs.xdg-desktop-portal-wlr
+      # pkgs.xdg-desktop-portal-gnome
     ];
     configPackages = [
       pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-hyprland
-      pkgs.xdg-desktop-portal-wlr
-      pkgs.xdg-desktop-portal-gnome
+      # pkgs.xdg-desktop-portal-wlr
+      # pkgs.xdg-desktop-portal-gnome
     ];
     config.common = {
       default = [
