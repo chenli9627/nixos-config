f4adfca621c42e9a72d0571b00368d4764235ae2
chenli9627 <chenli9627@gmail.com>
2025-07-14 18:33:58 +0800

    add kitty chinese font weight

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -1,4 +1,5 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   programs.kitty = pkgs.lib.mkForce {
     enable = true;
     themeFile = "GitHub_Light";
@@ -6,13 +7,14 @@
     settings = {
       font_family = "CaskaydiaMono Nerd Font Mono";
       enable_audio_bell = false;
-      symbol_map = let
-        mappings = [
-          "U+4E00-U+9FFF"
-          "U+3400-U+4DBF"
-        ];
-      in
-        (builtins.concatStringsSep "," mappings) + " Noto Sans CJK SC";
+      symbol_map =
+        let
+          mappings = [
+            "U+4E00-U+9FFF"
+            "U+3400-U+4DBF"
+          ];
+        in
+        (builtins.concatStringsSep "," mappings) + " Noto Sans CJK SC:weigh=600";
     };
   };
 }
