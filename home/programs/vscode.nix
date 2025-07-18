9ea383f3074451d7202de7c26ecca0296d9124da
chenli9627 <chenli9627@gmail.com>
2025-07-19 00:05:22 +0800

    use nix to manage vscode

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -1,45 +1,46 @@
+{ pkgs, ... }:
 {
   programs.vscode = {
     enable = true;
-    mutableExtensionsDir = true;
-    # profiles.default = {
-    #   enableExtensionUpdateCheck = false;
-    #   enableUpdateCheck = false;
-    #   extensions = with pkgs.vscode-extensions; [
-    #     yzhang.markdown-all-in-one
-    #     golang.go
-    #     myriad-dreamin.tinymist
-    #     github.github-vscode-theme
-    #     catppuccin.catppuccin-vsc
-    #     jnoortheen.nix-ide
-    #     scalameta.metals
-    #     ms-python.python
-    #     ms-python.debugpy
-    #     kamadorueda.alejandra
-    #     asvetliakov.vscode-neovim
-    #     ms-vscode.cpptools-extension-pack
-    #     ms-vscode.cpptools
-    #     ms-vscode.cmake-tools
-    #     mkhl.direnv
-    #     rust-lang.rust-analyzer
-    #     tomoki1207.pdf
-    #     ecmel.vscode-html-css
-    #     ms-azuretools.vscode-docker
-    #     redhat.java
-    #     vscjava.vscode-java-debug
-    #   ];
-    #   userSettings = {
-    #     "editor.fontFamily" = "CaskaydiaMono Nerd Font Mono";
-    #     "workbench.colorTheme" = "GitHub Light";
-    #     "explorer.confirmDelete" = false;
-    #     "explorer.confirmDragAndDrop" = false;
-    #     "extensions.autoUpdate" = false;
-    #     "update.mode" = "none";
-    #     # "nix.formatterPath" = "alejandra";
-    #     "extensions.experimental.affinity" = {
-    #       "asvetliakov.vscode-neovim" = 1;
-    #     };
-    #   };
+    mutableExtensionsDir = false;
+    profiles.default = {
+      enableExtensionUpdateCheck = false;
+      enableUpdateCheck = false;
+      extensions = with pkgs.vscode-extensions; [
+        yzhang.markdown-all-in-one
+        golang.go
+        myriad-dreamin.tinymist
+        github.github-vscode-theme
+        catppuccin.catppuccin-vsc
+        jnoortheen.nix-ide
+        scalameta.metals
+        ms-python.python
+        ms-python.debugpy
+        kamadorueda.alejandra
+        asvetliakov.vscode-neovim
+        ms-vscode.cpptools-extension-pack
+        ms-vscode.cpptools
+        ms-vscode.cmake-tools
+        mkhl.direnv
+        rust-lang.rust-analyzer
+        tomoki1207.pdf
+        ecmel.vscode-html-css
+        ms-azuretools.vscode-docker
+        redhat.java
+        vscjava.vscode-java-debug
+      ];
+      userSettings = {
+        "editor.fontFamily" = "CaskaydiaMono Nerd Font Mono";
+        "workbench.colorTheme" = "GitHub Light";
+        "explorer.confirmDelete" = false;
+        "explorer.confirmDragAndDrop" = false;
+        "extensions.autoUpdate" = false;
+        "update.mode" = "none";
+        # "nix.formatterPath" = "alejandra";
+        "extensions.experimental.affinity" = {
+          "asvetliakov.vscode-neovim" = 1;
+        };
+      };
+    };
   };
-  # };
 }
