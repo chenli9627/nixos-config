6dd6d8fb9d0b80efc1068e2550546690673ac265
chenli9627 <chenli9627@gmail.com>
2025-08-02 20:45:19 +0800

    change zathura config

--- home/programs/zathura.nix
+++ home/programs/zathura.nix
@@ -4,7 +4,9 @@
     mappings = {
       "<Right>" = "navigate next";
       # D = "toggle_page_mode";
-      "<Shift> D" = "toggle_page_mode";
+      "<C-d>" = "toggle_page_mode";
+      "D" = "scroll half-down";
+      "U" = "scroll half-up";
       "[fullscreen] <C-i>" = "zoom in";
     };
   };
