2c0ac3720a14e113cf5e877c61b71d7f8d2d9a59
chenli9627 <chenli9627@gmail.com>
2025-08-02 20:49:06 +0800

    jsd

--- home/programs/zathura.nix
+++ home/programs/zathura.nix
@@ -3,10 +3,10 @@
     enable = true;
     mappings = {
       "<Right>" = "navigate next";
-      # D = "toggle_page_mode";
-      "<C-d>" = "toggle_page_mode";
-      "D" = "scroll half-down";
-      "U" = "scroll half-up";
+      D = "toggle_page_mode";
+      # "<C-d>" = "toggle_page_mode";
+      "Space" = "scroll half-down";
+      "<S-Space>" = "scroll half-up";
       "[fullscreen] <C-i>" = "zoom in";
     };
   };
