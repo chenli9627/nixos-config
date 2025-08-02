f807b651f933e1200dec6d163898db95431eb173
chenli9627 <chenli9627@gmail.com>
2025-08-02 12:05:06 +0800

    no

--- modules/system.nix
+++ modules/system.nix
@@ -105,7 +105,7 @@
     git
     # clash-verge-rev
     curl
-    clash-nyanpasu
+    # clash-nyanpasu
     wget
     sqlite
   ];
@@ -124,8 +124,8 @@
 
   environment.variables.EDITOR = "nvim";
   programs = {
-    # clash-verge.enable = true;
-    # clash-verge.autoStart = true;
+    clash-verge.enable = true;
+    clash-verge.autoStart = true;
     fish.enable = true;
   };
 
