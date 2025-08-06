7b095654bb17521f989adfbc6913eb91999af2d8
chenli9627 <chenli9627@gmail.com>
2025-08-06 13:15:29 +0800

    remove iwd

--- hosts/lenovo-laptop/default.nix
+++ hosts/lenovo-laptop/default.nix
@@ -56,5 +56,4 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
-  networking.wireless.iwd.enable = true;
 }
