732c9a13133be59c1547d3e0ecf691451b6dff46
chenli9627 <chenli9627@gmail.com>
2025-08-09 01:20:54 +0800

    disable nvme powermanagement by add some kernel parameters

--- hosts/lenovo-laptop/default.nix
+++ hosts/lenovo-laptop/default.nix
@@ -30,6 +30,11 @@
   };
   boot.kernelParams = [
     "amdgpu.dcdebugmask=0x10"
+    # "nvme.noacpi=1"
+    "nvme_core.default_ps_max_latency_us=0"
+    # "iommu=soft"
+    "pcie_aspm=off"
+    "pcie_port_pm=off"
   ];
 
   # boot = {
