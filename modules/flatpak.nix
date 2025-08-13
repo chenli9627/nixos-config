41450ff3f6b6580707651ff02b8461831c86e536
chenli9627 <chenli9627@gmail.com>
2025-08-14 02:04:26 +0800

    add flathub

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -4,11 +4,11 @@
     enable = true;
   };
 
-  # systemd.services.flatpak-repo = {
-  #   wantedBy = [ "multi-user.target" ];
-  #   path = [ pkgs.flatpak ];
-  #   script = ''
-  #     flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-  #   '';
-  # };
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
 }
