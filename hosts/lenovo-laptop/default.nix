7671528343d74ca70d2c7dc186f66e8a5bdb1876
chenli9627 <chenli9627@gmail.com>
2025-08-15 14:07:50 +0800

    network settings

--- hosts/lenovo-laptop/default.nix
+++ hosts/lenovo-laptop/default.nix
@@ -58,8 +58,17 @@
   #   };
   # };
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  # networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
+  networking = {
+    useDHCP = false;
+    hostName = "nixos"; # Define your hostname.
+    # Pick only one of the below networking options.
+    wireless.enable = true; # Enables wireless support via wpa_supplicant.
+    networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  };
 }
