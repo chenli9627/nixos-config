d8d4dcb8f8196a0a74a40a73f3174bbec6ff482e
chenli9627 <chenli9627@gmail.com>
2025-08-20 15:06:45 +0800

    a refactor of wps desktopEntries by kimi

--- home/hyprland/desktopEntries.nix
+++ home/hyprland/desktopEntries.nix
@@ -1,34 +1,81 @@
+{ pkgs, ... }:
+
+let
+  launchScript =
+    pkg:
+    pkgs.writeShellScriptBin "${pkg}-scaled" ''
+      export QT_FONT_DPI=144
+      export LANG=zh_CN.UTF-8
+      exec ${pkg} "$@"
+    '';
+in
 {
   xdg.desktopEntries = {
     wps = {
-      name = "WPS Office(scaled)";
-      genericName = "doc editor";
-      exec = "export QT_FONT_DPI=144 && export LANG=zh_CN.UTF-8 && wps";
+      name = "WPS Office (scaled)";
+      genericName = "Office Suite";
+      exec = "${launchScript "wps"}/bin/wps-scaled %F";
       terminal = false;
       categories = [
-        "Utility"
-        "Editor"
+        "Office"
+        "WordProcessor"
       ];
     };
+
     wpp = {
-      name = "WPS Presentation(scaled)";
-      genericName = "doc editor";
-      exec = "export QT_FONT_DPI=144 && export LANG=zh_CN.UTF-8 && wpp";
+      name = "WPS Presentation (scaled)";
+      genericName = "Presentation";
+      exec = "${launchScript "wpp"}/bin/wpp-scaled %F";
       terminal = false;
       categories = [
-        "Utility"
-        "Editor"
+        "Office"
+        "Presentation"
       ];
     };
+
     wpspdf = {
-      name = "WPS PDF(scaled)";
-      genericName = "doc editor";
-      exec = "export QT_FONT_DPI=144 && export LANG=zh_CN.UTF-8 && wpspdf";
+      name = "WPS PDF (scaled)";
+      genericName = "PDF Viewer";
+      exec = "${launchScript "wpspdf"}/bin/wpspdf-scaled %F";
       terminal = false;
       categories = [
-        "Utility"
-        "Editor"
+        "Office"
+        "Viewer"
       ];
     };
   };
 }
+# {
+#   xdg.desktopEntries = {
+#     wps = {
+#       name = "WPS Office(scaled)";
+#       genericName = "doc editor";
+#       exec = "export QT_FONT_DPI=144 && export LANG=zh_CN.UTF-8 && wps";
+#       terminal = false;
+#       categories = [
+#         "Utility"
+#         "Editor"
+#       ];
+#     };
+#     wpp = {
+#       name = "WPS Presentation(scaled)";
+#       genericName = "doc editor";
+#       exec = "export QT_FONT_DPI=144 && export LANG=zh_CN.UTF-8 && wpp";
+#       terminal = false;
+#       categories = [
+#         "Utility"
+#         "Editor"
+#       ];
+#     };
+#     wpspdf = {
+#       name = "WPS PDF(scaled)";
+#       genericName = "doc editor";
+#       exec = "export QT_FONT_DPI=144 && export LANG=zh_CN.UTF-8 && wpspdf";
+#       terminal = false;
+#       categories = [
+#         "Utility"
+#         "Editor"
+#       ];
+#     };
+#   };
+# }
