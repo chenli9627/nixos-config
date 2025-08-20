858b254f4825348fc3e0e9f72ec2691c9bd96081
chenli9627 <chenli9627@gmail.com>
2025-08-20 15:03:33 +0800

    skd;ask

--- home/hyprland/desktopEntries.nix
+++ home/hyprland/desktopEntries.nix
@@ -2,7 +2,7 @@
   xdg.desktopEntries = {
     wps = {
       name = "WPS Office(scaled)";
-      genericname = "doc editor";
+      genericName = "doc editor";
       exec = "export QT_FONT_DPI=144 && export LANG=zh_CN.UTF-8 && wps";
       terminal = false;
       categories = [
@@ -12,7 +12,7 @@
     };
     wpp = {
       name = "WPS Presentation(scaled)";
-      genericname = "doc editor";
+      genericName = "doc editor";
       exec = "export QT_FONT_DPI=144 && export LANG=zh_CN.UTF-8 && wpp";
       terminal = false;
       categories = [
@@ -22,7 +22,7 @@
     };
     wpspdf = {
       name = "WPS PDF(scaled)";
-      genericname = "doc editor";
+      genericName = "doc editor";
       exec = "export QT_FONT_DPI=144 && export LANG=zh_CN.UTF-8 && wpspdf";
       terminal = false;
       categories = [
