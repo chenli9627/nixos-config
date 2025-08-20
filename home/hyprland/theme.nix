6505cf02ff440f6f36f3b604bb5984dbe2592f3c
chenli9627 <chenli9627@gmail.com>
2025-08-20 14:04:22 +0800

    dont use   xresources.properties

--- home/hyprland/theme.nix
+++ home/hyprland/theme.nix
@@ -46,19 +46,19 @@
     #    defaultCursor = "Adwaita";
     # };
   };
-  xresources.properties = {
-    "Xft.dpi" = 192; # 根据缩放比例调整（96 × 缩放系数）
-
-    # ! These might also be useful depending on your monitor and personal preference:
-    "Xft.autohint" = 0;
-    "Xft.lcdfilter" = "lcddefault";
-    "Xft.hintstyle" = "hintfull";
-    "Xft.hinting" = 1;
-    "Xft.antialias" = 1;
-    "Xft.rgba" = "rgb";
-    # I wish these above may effect how xwayland apps behaves
-    # update: it runs well😄
-  };
+  # xresources.properties = {
+  #   "Xft.dpi" = 192; # 根据缩放比例调整（96 × 缩放系数）
+  #
+  #   # ! These might also be useful depending on your monitor and personal preference:
+  #   "Xft.autohint" = 0;
+  #   "Xft.lcdfilter" = "lcddefault";
+  #   "Xft.hintstyle" = "hintfull";
+  #   "Xft.hinting" = 1;
+  #   "Xft.antialias" = 1;
+  #   "Xft.rgba" = "rgb";
+  #   # I wish these above may effect how xwayland apps behaves
+  #   # update: it runs well😄
+  # };
   # services.xsettingsd = {
   #   enable = true;
   #   settings = {
