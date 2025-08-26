2280b04a417658106a26d1044a41d35feed91038
chenli9627 <chenli9627@gmail.com>
2025-08-26 22:13:15 +0800

    reset kernel para  "nvme_core.default_ps_max_latency_us=0"

--- hosts/lenovo-laptop/default.nix
+++ hosts/lenovo-laptop/default.nix
@@ -32,7 +32,7 @@
   boot.kernelParams = [
     "amdgpu.dcdebugmask=0x10"
     # "nvme.noacpi=1" # seems to break something
-    # "nvme_core.default_ps_max_latency_us=0"
+    "nvme_core.default_ps_max_latency_us=0"
     # "iommu=soft"
     # "pcie_aspm=off"
     # "pcie_port_pm=off"
