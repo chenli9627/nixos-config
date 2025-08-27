85dbb92cc22ad00fc413e141e12f5d6e9d152f44
chenli9627 <chenli9627@gmail.com>
2025-08-27 22:17:01 +0800

    satty

--- home/programs/satty.nix
+++ home/programs/satty.nix
@@ -1,23 +1,23 @@
 {
   programs.satty = {
     enable = true;
-    settings = {
-      general = {
-        # fullscreen = true;
-        corner-roundness = 12;
-        initial-tool = "brush";
-        output-filename = "~/Pictures/screenshots/satty-%Y-%m-%d_%H:%M:%S.png";
-      };
-      color-palette = {
-        palette = [
-          "#00ffff"
-          "#a52a2a"
-          "#dc143c"
-          "#ff1493"
-          "#ffd700"
-          "#008000"
-        ];
-      };
-    };
+    # settings = {
+    #   general = {
+    #     # fullscreen = true;
+    #     corner-roundness = 12;
+    #     initial-tool = "brush";
+    #     output-filename = "~/Pictures/screenshots/satty-%Y-%m-%d_%H:%M:%S.png";
+    #   };
+    #   color-palette = {
+    #     palette = [
+    #       "#00ffff"
+    #       "#a52a2a"
+    #       "#dc143c"
+    #       "#ff1493"
+    #       "#ffd700"
+    #       "#008000"
+    #     ];
+    #   };
+    # };
   };
 }
