ce496daa0f3523228654dfc1a749a6f8002b7f4b
chenli9627 <chenli9627@gmail.com>
2025-09-03 14:40:21 +0800

    add mongodb settings

--- modules/databases.nix
+++ modules/databases.nix
@@ -1,6 +1,11 @@
 { lib, pkgs, ... }:
 {
-  systemd.services.mongodb.wantedBy = lib.mkForce [ ];
+  systemd.services.mongodb = {
+    wantedBy = lib.mkForce [ ];
+    environment = {
+      GLIBC_TUNABLES = "glibc.pthread.rseq=0";
+    };
+  };
   services.mongodb = {
     enable = true;
     package = pkgs.mongodb-ce;
