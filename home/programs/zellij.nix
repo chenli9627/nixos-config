75cab5b4143de115ecea2a6f5ef79045db0444be
chenli9627 <chenli9627@gmail.com>
2025-09-05 00:51:24 +0800

    add zellij-nav.nvim

--- home/programs/zellij.nix
+++ home/programs/zellij.nix
@@ -7,6 +7,25 @@
       auto_start = false;
       theme = "catppuccin-macchiato";
       # default_layout = "compact";
+      autolock = {
+        location = "builtin:autolock";
+        triggers = "nvim|vim|v|nv";
+        watch_triggers = "fzf|zoxide|atuin|atac";
+        watch_interval = "1.0";
+      };
     };
+    plugins = [
+      {
+        name = "autolock";
+        url = "https://github.com/fresh2dev/zellij-autolock/releases/latest/download/zellij-autolock.wasm";
+      }
+    ];
   };
 }
+# ...
+# autolock location="https://github.com/fresh2dev/zellij-autolock/releases/latest/download/zellij-autolock.wasm" {
+#     triggers "nvim|vim|v|nv"  // Lock when any open these programs open. They are expected to unlock themselves when closed (e.g., using zellij.vim plugin).
+#     watch_triggers "fzf|zoxide|atuin|atac"  // Lock when any of these open and monitor until closed.
+#     watch_interval "1.0"  // When monitoring, check every X seconds.
+# }
+# ...
