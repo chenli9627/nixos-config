919ce8a4a9a739c351d81513bb796ce89b93ed27
chenli9627 <chenli9627@gmail.com>
2025-09-08 19:47:55 +0800

    add zen-browser and its flake

--- home/programs/zen-browser.nix
+++ home/programs/zen-browser.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ pkgs, inputs, ... }:
 
 let
   zenRelease = "beta"; # beta, twilight, twilight-official
@@ -8,6 +8,25 @@ in
 
   programs.zen-browser = {
     enable = true;
+    nativeMessagingHosts = [ pkgs.firefoxpwa ];
+    policies = {
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
     # profiles."default" = {
     #   id = 0;
     #   isDefault = true;
