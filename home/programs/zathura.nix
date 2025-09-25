74120b8e6a72f045962db61154d8f018226a4f74
chenli9627 <chenli9627@gmail.com>
2025-09-25 17:27:33 +0800

    remove zathura config symlink

--- home/programs/zathura.nix
+++ home/programs/zathura.nix
@@ -1,13 +1,5 @@
 {
   programs.zathura = {
     enable = true;
-    mappings = {
-      # "<Right>" = "navigate next";
-      # D = "toggle_page_mode";
-      # # "<C-d>" = "toggle_page_mode";
-      # "Space" = "scroll half-down";
-      # "<S-Space>" = "scroll half-up";
-      # "[fullscreen] <C-i>" = "zoom in";
-    };
   };
 }
