5dd2a595f5d6de621e8b6cc9e970f2d8983b6b02
chenli9627 <chenli9627@gmail.com>
2025-10-13 18:40:41 +0800

    use extraHosts to block some websites

--- modules/network.nix
+++ modules/network.nix
@@ -17,4 +17,17 @@
     };
   };
 
+  networking.extraHosts = "
+    0.0.0.0 www.bilibili.com
+    0.0.0.0 t.bilibili.com
+    0.0.0.0 live.bilibili.com
+    0.0.0.0 api.bilibili.com
+    0.0.0.0 data.bilibili.com
+    0.0.0.0 api.live.bilibili.com
+    0.0.0.0 i0.hdslb.com
+    0.0.0.0 www.douyin.com
+    0.0.0.0 weibo.com
+    0.0.0.0 www.v2ex.com
+    0.0.0.0 www.xiaohongshu.com
+    ";
 }
