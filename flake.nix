c55ec7d0991c5c3aab1ac4eda52f71e12df957c8
chenli9627 <chenli9627@gmail.com>
2025-10-18 21:52:27 +0800

    add a hyprland plugin "hyprscrolling"

--- flake.nix
+++ flake.nix
@@ -16,6 +16,10 @@
       url = "github:nix-community/stylix/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
   };
 
   outputs =
--- home/hyprland/plugins.nix
+++ home/hyprland/plugins.nix
@@ -2,8 +2,12 @@
   pkgs,
   inputs,
   ...
-}: {
+}:
+{
+  # wayland.windowManager.hyprland.plugins = with pkgs.hyprlandPlugins; [
+  #   hyprscrolling
+  # ];
   wayland.windowManager.hyprland.plugins = [
-    # pkgs.hyprlandPlugins.<plugin>
+    inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprscrolling
   ];
 }
