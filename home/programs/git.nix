af4b944602cedb445a6f1db9ce16beb835630039
chenli9627 <chenli9627@gmail.com>
2025-11-05 17:58:54 +0800

    noting

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,7 +1,12 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   programs.git = {
     enable = true;
     userName = "chenli9627";
     userEmail = "chenli9627@gmail.com";
+    # settings = {
+    #   user.email = "chenli9627@gmail.com";
+    #   user.name = "chenli9627";
+    # };
   };
 }
--- home/programs/ssh.nix
+++ home/programs/ssh.nix
@@ -9,5 +9,31 @@
         port = 443;
       };
     };
+    # enableDefaultConfig = true;
+    # matchBlocks = [
+    #   {
+    #     addKeysToAgent = "yes";
+    #   }
+    # ];
+    # enableDefaultConfig = false;
+    # matchBlocks = {
+    #   "*" = {
+    #     forwardAgent = false;
+    #     addKeysToAgent = "yes";
+    #     compression = false;
+    #     serverAliveInterval = 0;
+    #     serverAliveCountMax = 3;
+    #     hashKnownHosts = false;
+    #     userKnownHostsFile = "~/.ssh/known_hosts";
+    #     controlMaster = "no";
+    #     controlPath = "~/.ssh/master-%r@%n:%p";
+    #     controlPersist = "no";
+    #   };
+    #   "github.com" = {
+    #     hostname = "ssh.github.com";
+    #     user = "git";
+    #     port = 443;
+    #   };
+    # };
   };
 }
