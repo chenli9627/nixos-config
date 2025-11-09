e125ea461ce1a6a949c38264c33cdf35d473decb
chenli9627 <chenli9627@gmail.com>
2025-11-09 22:07:22 +0800

    uncomment hyprland cache key

--- modules/system.nix
+++ modules/system.nix
@@ -35,7 +35,7 @@
         "https://cache.garnix.io"
       ];
       trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
       experimental-features = "nix-command flakes";
