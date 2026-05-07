ca67f5130025623aa1ce2757ed8ba6c59a526a3f
chenli9627 <chenli9627@gmail.com>
2026-05-08 02:12:56 +0800

    set tmux to use fish

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -1,4 +1,10 @@
-{programs.tmux = {
+{pkgs,...}:{programs.tmux = {
 enable = true;
+aggressiveResize = true;
+keyMode = "vi";
+mouse = true;
+shell = "${pkgs.fish}/bin/fish";
+shortcut = "a";
+
 };
 }
