d25f0af7bc8fab3a71650501cdf7baf78b519d77
chenli9627 <chenli9627@gmail.com>
2026-06-11 22:58:31 +0800

    update ssh config for 26.05

--- home/programs/ssh.nix
+++ home/programs/ssh.nix
@@ -2,21 +2,8 @@
   programs.ssh = {
     # addKeysToAgent = "yes";
     enable = true;
-    # matchBlocks = {
-    #   "github.com" = {
-    #     hostname = "ssh.github.com";
-    #     user = "git";
-    #     port = 443;
-    #   };
-    # };
     enableDefaultConfig = false;
-    # matchBlocks = [
-    #   {
-    #     addKeysToAgent = "yes";
-    #   }
-    # ];
-    # enableDefaultConfig = false;
-    matchBlocks = {
+    settings = {
       "*" = {
         # forwardAgent = false;
         addKeysToAgent = "yes";
